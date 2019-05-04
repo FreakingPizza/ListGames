93abbf80e9f0d1aa41a76da922fe3e13f29a0292
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-03 22:49:23 -0300

    Initial commit

---
+++ app/src/main/java/fiap/com/br/meusgames/ListaGamesActivity.kt
@@ -0,0 +1,34 @@
+package fiap.com.br.meusgames
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import android.widget.Toast
+import fiap.com.br.meusgames.model.Jogo
+import kotlinx.android.synthetic.main.activity_lista_games.*
+
+class ListaGamesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_games)
+    rvJogos.adapter = ListaGamesAdapter(meusJogos(),this,{
+        Toast.makeText(this,
+                        it.nome,
+                        Toast.LENGTH_LONG).show()
+    })
+
+        rvJogos.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    fun meusJogos() :List<Jogo>{
+        return listOf(
+                Jogo(R.drawable.doom, "DOOM",  2019, "Aqui vou falar do jogo")
+        )
+    }
+
+
+
+}
---
+++ app/src/main/java/fiap/com/br/meusgames/model/Jogo.kt
@@ -0,0 +1,8 @@
+package fiap.com.br.meusgames.model
+
+data class Jogo(
+        val resourceId: Int,
+        val nome: String,
+        val anoLancamento: Int,
+        val descricao: String
+)
